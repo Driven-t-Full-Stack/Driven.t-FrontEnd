b50e25e7ed8793151e668ec67e52778896c8329b
Alan Timbó <alancorreiatimbo@gmail.com>
2023-05-15 04:00:52 -0300

    style: change in payment confirmation message style

--- src/components/Dashboard/Payment/SuccessfulMessage.js
+++ src/components/Dashboard/Payment/SuccessfulMessage.js
@@ -1,11 +1,31 @@
+import { AiFillCheckCircle } from 'react-icons/ai';
+import styled from 'styled-components';
+
 export default function SuccessfulMessage() {
   return (
-    <div>
+    <SuccessfulPaymentMessage>
+      <AiFillCheckCircle style={{ color: '#36B853', fontSize: '44px' }} />
       <h1>
         <strong>Pagamento Confirmado! </strong> <br />
         Prossiga para a escolha de hospedagem e atividades
       </h1>
       <p></p>
-    </div>
+    </SuccessfulPaymentMessage>
   );
 }
+
+const SuccessfulPaymentMessage = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  h1 {
+    margin-left: 12px;
+
+    color: #000;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 18.75px;
+  }
+`;
