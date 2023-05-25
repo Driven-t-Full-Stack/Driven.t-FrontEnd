2baed60cced3b7b7f6de434c094d48420ba7c79a
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2023-05-25 08:56:59 -0300

    refacotr/ begin activities code

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,27 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  return (
+    <ActivitiesAreaContainer>
+      <Title>Escolha de atividades</Title>
+    </ActivitiesAreaContainer>
+  );
 }
+
+const ActivitiesAreaContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  
+`;
+
+const Title = styled.h1`
+ font-family: 'Roboto';
+ font-style: normal;
+ font-weight: 400;
+ font-size: 34px;
+ line-height: 40px;
+ color: #000000;
+ height: auto;
+ margin-bottom: 35px;
+`;
